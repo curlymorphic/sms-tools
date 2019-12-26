e3f71c6d26329e40e7de175c6a042f75da20fc0e
Curlymorphic <dave.french3@googlemail.com>
2019-12-26 12:30:50 +0000

    Week 1 & 2

---
+++ workspace/A1/A1Part3.py
@@ -0,0 +1,39 @@
+"""
+A1-Part-3: Python array indexing
+
+Write a function that given a numpy array x, returns every Mth element in x, starting from the 
+first element.  
+
+The input arguments to this function are a numpy array x and a positive integer M such that M < number of 
+elements in x. The output of this function should be a numpy array.
+
+If you run your code with x = np.arange(10) and M = 2, the function should return the following output: 
+array([0, 2, 4, 6, 8]).
+"""
+def hopSamples(x,M):
+    """
+    Inputs:
+        x: input numpy array
+        M: hop size (positive integer)
+    Output:
+        A numpy array containing every Mth element in x, starting from the first element in x.
+    """
+    ## Your code here
+
+    import numpy as np
+    import math
+    outsize= int(math.ceil(len(x)/float(M)))
+    out=np.array([0])
+    out = np.resize(out,0)
+ 
+        
+    i = 0
+    while (i<len(x)):
+        out = np.append(out, [x[i]], axis=0)
+        i = i + M
+        
+    return out
+
+    
+
+  
